27b7f1b425d5ae8cb7c0601a11d2f5a09b65ad4a
Alexey Savkin <savkinshot@gmail.com>
2015-08-05 16:30:49 +0300

    version 0.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -13,30 +13,35 @@ var gulp = require('gulp'),
 	rimraf = require('rimraf'),
 	plumber = require('gulp-plumber'),
 	browserSync = require("browser-sync"),
-	reload = browserSync.reload;
+	reload = browserSync.reload,
+	spritesmith  = require('gulp.spritesmith');
 
 // Пути для всех файлов проекта
 var path = {
 	build: { 
 		html: 'build/',
-		js: 'build/js/',
-		css: 'build/css/',
-		img: 'build/img/',
-		fonts: 'build/fonts/'
+		js: './build/js/',
+		css: './build/css/',
+		img: './build/img/',
+		spriteImg: './build/img/',
+		spriteSass: './src/style/2-base/',
+		fonts: './build/fonts/'
 	},
 	src: {
-		html: 'src/index.jade',
-		js: 'src/js/main.js',
-		style: 'src/style/common.sass',
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/**/*.*'
+		html: './src/index.jade',
+		js: './src/js/main.js',
+		style: './src/style/common.sass',
+		sprite: './src/img/sprite/*.*',
+		img: ['./src/img/**/*.*', '!./src/img/sprite/*.*'],
+		fonts: './src/fonts/**/*.*'
 	},
 	watch: { 
-		html: 'src/**/*.jade',
-		js: 'src/js/**/*.js',
-		style: ['src/style/**/*.sass', 'src/style/**/*.scss'],
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/**/*.*'
+		html: './src/**/*.jade',
+		js: './src/js/**/*.js',
+		style: ['./src/style/**/*.sass', './src/style/**/*.scss'],
+		sprite: './src/img/sprite/*.*',
+		img: ['./src/img/**/*.*', '!./src/img/sprite/*.*'],
+		fonts: './src/fonts/**/*.*'
 	},
 	clean: './build'
 };
@@ -78,24 +83,45 @@ gulp.task('js:build', function () {
 // Style build
 gulp.task('style:build', function () {
 	gulp.src(path.src.style)
-		.pipe(sourcemaps.init())
+		.pipe(plumber())
+		// .pipe(sourcemaps.init())
 		.pipe(sass({
-			indentedSyntax: true,
-			errLogToConsole: true
+			indentedSyntax: true
 		}))
 		.pipe(autoprefixer({
 			browsers: ['last 15 version', '> 1%', 'ie 8'],
 			cascade: true
 		}))
-		.pipe(cssmin())
-		.pipe(sourcemaps.write())
+		// .pipe(cssmin())
+		// .pipe(sourcemaps.write())
 		.pipe(gulp.dest(path.build.css))
 		.pipe(reload({stream: true}));
+});
+
+// Sprite Build
+gulp.task('sprite:build', function() {
+	var spriteData = 
+		gulp.src(path.src.sprite) // путь, откуда берем картинки для спрайта
+			.pipe(plumber())
+			.pipe(spritesmith({
+				imgName: 'sprite.png',
+				cssName: '_sprite.sass',
+				cssFormat: 'sass',
+				algorithm: 'binary-tree',
+				cssTemplate: './src/style/2-base/sass.template.mustache',
+				cssVarMap: function(sprite) {
+					sprite.name = 's-' + sprite.name
+				}
+			}));
+
+	spriteData.img.pipe(gulp.dest(path.build.spriteImg)); // путь, куда сохраняем картинку
+	spriteData.css.pipe(gulp.dest(path.build.spriteSass)); // путь, куда сохраняем стили
 });
 
 // Image build
 gulp.task('image:build', function () {
 	gulp.src(path.src.img)
+		.pipe(plumber())
 		.pipe(imagemin({ 
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
@@ -109,6 +135,7 @@ gulp.task('image:build', function () {
 // Fonts build
 gulp.task('fonts:build', function() {
 	gulp.src(path.src.fonts)
+		.pipe(plumber())
 		.pipe(gulp.dest(path.build.fonts))
 });
 
@@ -118,6 +145,7 @@ gulp.task('build', [
 	'js:build',
 	'style:build',
 	'fonts:build',
+	'sprite:build',
 	'image:build'
 ]);
 
@@ -127,6 +155,7 @@ gulp.task('watch', function () {
 	gulp.watch(path.watch.style, ['style:build']);
 	gulp.watch(path.watch.js, ['js:build']);
 	gulp.watch(path.watch.img, ['image:build']);
+	gulp.watch(path.watch.sprite, ['sprite:build']);
 	gulp.watch(path.watch.fonts, ['fonts:build']);
 });
 
@@ -135,9 +164,11 @@ gulp.task('webserver', function () {
 	browserSync(config);
 });
 
+// Clean task
 gulp.task('clean', function (cb) {
 	rimraf(path.clean, cb);
 });
 
 // Default task
 gulp.task('default', ['build', 'webserver', 'watch']);
+
